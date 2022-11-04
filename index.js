258a8270944e42ee4549d5964249ea0d01b0981f
Vasyl-FE <109676846+Vasyl-FE@users.noreply.github.com>
2022-11-04 20:12:53 +0200

    Finalized HW

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const num1 = +prompt("Enter first number:");
 const num2 = +prompt("Enter second number:");
 
-alert(`The smallest value is ${min(num1, num2)}`);
+alert(`The smallest value of two entered numbers is ${min(num1, num2)}`);
 
 function min(a, b) {
     if (a < b) {
