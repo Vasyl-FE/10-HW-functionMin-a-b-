f5b07390c275f4e6b6e5eedd351d7245a44e809f
Vasyl S <sukhovoy.v@gmail.com>
2022-11-04 19:56:53 +0200

    HW upload

---
+++ index.js
@@ -0,0 +1,12 @@
+const num1 = prompt("Enter first number:");
+const num2 = prompt("Enter second number:");
+
+alert(`The smallest value is ${min(num1, num2)}`);
+
+function min(a, b) {
+    if (a < b) {
+        return a;
+    } else {
+        return b;
+    }
+}
